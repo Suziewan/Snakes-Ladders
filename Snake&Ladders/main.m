c8c54ca8f227a2b5a155e9ae82b2ad528448c67c
Suziewan <angielinton@me.com>
2017-01-14 10:29:27 -0500

    Initial Commit

---
+++ Snake&Ladders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Snake&Ladders
+//
+//  Created by Angie Linton on 2017-01-14.
+//  Copyright © 2017 Angie Linton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
